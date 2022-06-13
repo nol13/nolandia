33170ebb6d9382e0918ad128a2ecbc0b7fbe81aa
nolan <nolan@nolankaplan.com>
2022-06-13 03:04:59 -0400

    test

--- contracts/MetaNol2.sol
+++ contracts/MetaNol2.sol
@@ -5,9 +5,9 @@ pragma solidity ^0.8.0;
 
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-//import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+// import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GameItem is ERC721/* , ERC721Enumerable, ERC721Royalty, Ownable */ {
 
