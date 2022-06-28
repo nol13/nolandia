997443cf7b7d37d1b6ae3576c964b95e304ea72c
nolan <nolan@nolankaplan.com>
2022-06-28 16:11:55 -0400

    add call to mint and report number owned

--- frontend/src/components/MintPlot/MintPlot.tsx
+++ frontend/src/components/MintPlot/MintPlot.tsx
@@ -1,14 +1,59 @@
-import { useState } from "react";
-//import { useWeb3Contract } from "react-moralis";
-//0x4A5B2494CBae765766684dC7F58fF381f2C756B4
+import { useState, useEffect } from "react";
+import { useWeb3Contract, useWeb3ExecuteFunction, useMoralis } from "react-moralis";
+import NolandiaAbi from '../../contracts/Nolandia.json';
+import contractAddress from "../../contracts/contract-address.json";
+//  "Nolandia": "0x4A5B2494CBae765766684dC7F58fF381f2C756B4"
 
 
 export const MintPlot = () => {
-    const mint = () => {};
-    const [x1, setX1] = useState<string>();
-    const [y1, setY1] = useState<string>();
-    const [x2, setX2] = useState<string>();
-    const [y2, setY2] = useState<string>();
+    const { user } = useMoralis();
+    const { data, error, runContractFunction, isFetching, isLoading } =
+        useWeb3Contract({
+            abi: NolandiaAbi.abi,
+            contractAddress: contractAddress.Nolandia,
+            functionName: "buyPlot"
+        });
+
+    //const balance = runContractFunction({params: {}})
+    const { data: balanceData, error: bError, fetch, /* isFetching, isLoading */ } = useWeb3ExecuteFunction({
+        abi: NolandiaAbi.abi,
+        contractAddress: contractAddress.Nolandia,
+        functionName: "balanceOf",
+        params: {
+            owner: user?.get("ethAddress")
+        },
+    });
+
+    useEffect(() => {
+        fetch();
+    }, [fetch])
+ 
+    
+    const [x1, setX1] = useState<string>("");
+    const [y1, setY1] = useState<string>("");
+    const [x2, setX2] = useState<string>("");
+    const [y2, setY2] = useState<string>("");
+
+    const mint = () => {
+        const x1Int = parseInt(x1);
+        const y1Int = parseInt(y1);
+        const x2Int = parseInt(x2);
+        const y2Int = parseInt(y2);
+
+        if (
+            Number.isInteger(x1Int) &&
+            Number.isInteger(y1Int) &&
+            Number.isInteger(x2Int) &&
+            Number.isInteger(y2Int)
+        ) {
+            const value = ((x2Int - x1Int) * (y2Int - y1Int)) * 64
+            const options = {
+                params: { x1, y1, x2, y2 },
+                msgValue: `${value}`
+            }
+            runContractFunction({ params: options }).then(() => fetch());
+        }
+    };
     return (
         <div>
             <h1>Mint New Plot</h1>
@@ -16,7 +61,12 @@ export const MintPlot = () => {
             <div>y1: <input value={y1} onChange={e => setY1(e.target.value)} /></div>
             <div>x2: <input value={x2} onChange={e => setX2(e.target.value)} /></div>
             <div>x2: <input value={y2} onChange={e => setY2(e.target.value)} /></div>
-            <div><button onClick={mint}>Mint!</button></div>
+            <div><button disabled={isFetching || isLoading} onClick={mint}>Mint!</button></div>
+            <div>data: {JSON.stringify(data)}</div>
+            <div>error: {JSON.stringify(error)}</div>
+            <div>you own: {JSON.stringify(balanceData)}</div>
+            <div>b error: {JSON.stringify(bError)}</div>
+           
         </div>
     )
 };
