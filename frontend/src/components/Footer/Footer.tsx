77851dd389bacf438c98fe996e80730c02f663a4
Elise A <technrd@protonmail.com>
2022-07-20 18:00:25 -0500

    Added Footer component and its css file and imported Footer component to Home component.

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classnames from "classnames";
+
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+
+    return (
+        <footer className={styles.footer}>
+            <p>Copyright &copy; 2022 Nolandia. All Rights Reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
