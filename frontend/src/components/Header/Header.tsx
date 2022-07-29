29ad652edf84f91b49fd38d68293b465e3708f8b
Nolan Kaplan <nolan@nolankaplan.com>
2022-07-28 23:43:07 -0400

    add temporary nav links

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from "classnames";
+import {Link} from "react-router-dom";
 
 import styles from './Header.module.scss';
 
@@ -13,8 +14,12 @@ const Header = ({
         <header className={styles.header}>
             <div className={classnames("wrapper", styles.container)}>
                 <h1><a href="/" className={styles.logo}>nolandia</a></h1>
-                {isAuth ? (
-                    <button className="standard-btn" onClick={logout}>Logout</button>
+                {isAuth ? (<div>
+                        <Link className={styles.headerLink} to={`/`}>Home</Link>
+                        <Link className={styles.headerLink} to={`/myplots`}>My Plots</Link>
+                        <Link className={styles.headerLink} to={`/buyplot`}>Purchase Plot</Link>
+                        <button className="standard-btn" onClick={logout}>Logout</button>
+                    </div>
                 ) : (
                     <button className="standard-btn" onClick={login}>Connect Wallet</button>
                 )}
